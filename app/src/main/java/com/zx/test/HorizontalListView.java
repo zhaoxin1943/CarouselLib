2a83f620bf5df3feaa9583163bc434670eeb5f5c
Abner <nimengbo@souche.com>
2015-01-23 13:29:20 +0800

    setSelection

--- app/src/main/java/com/zx/test/HorizontalListView.java
+++ app/src/main/java/com/zx/test/HorizontalListView.java
@@ -20,136 +20,35 @@ import java.util.Queue;
 
 
 public class HorizontalListView extends AdapterView<ListAdapter> {
-    private static final float TO_SCALE = 1.2f;
-    /**
-     * 每个item之间的距离，相当于dividetWidth
-     */
-    private static final int CHILD_MARGIN = 40;
-    private static final int PADDING_TOP = 30;
     public boolean mAlwaysOverrideTouch = true;
     protected ListAdapter mAdapter;
-    protected int mCurrentX;
-    protected int mNextX;
-    protected Scroller mScroller;
     private int mLeftViewIndex = -1;
     private int mRightViewIndex = 0;
+    protected int mCurrentX;
+    protected int mNextX;
     private int mMaxX = Integer.MAX_VALUE;
     private int mDisplayOffset = 0;
+    protected Scroller mScroller;
     private GestureDetector mGesture;
     private Queue<View> mRemovedViewQueue = new LinkedList<>();
     private OnItemSelectedListener mOnItemSelected;
     private OnItemClickListener mOnItemClicked;
     private OnItemLongClickListener mOnItemLongClicked;
-    private OnGestureListener mOnGesture = new GestureDetector.SimpleOnGestureListener() {
-
-        @Override
-        public boolean onDown(MotionEvent e) {
-            return HorizontalListView.this.onDown(e);
-        }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-                               float velocityY) {
-            return HorizontalListView.this.onFling(e1, e2, velocityX, velocityY);
-        }
-
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2,
-                                float distanceX, float distanceY) {
-
-            synchronized (HorizontalListView.this) {
-                mNextX += (int) distanceX;
-            }
-            requestLayout();
-
-            return true;
-        }
-
-        /**
-         * 点击动作执行完毕时会调用
-         * @param e
-         * @return
-         */
-        @Override
-        public boolean onSingleTapConfirmed(MotionEvent e) {
-            for (int i = 0; i < getChildCount(); i++) {
-                View child = getChildAt(i);
-                if (isEventWithinView(e, child)) {
-                    if (mOnItemClicked != null) {
-                        mOnItemClicked.onItemClick(HorizontalListView.this, child, mLeftViewIndex + 1 + i, mAdapter.getItemId(mLeftViewIndex + 1 + i));
-                    }
-//                    setSelection(mLeftViewIndex + 1 + i);
-                    if (mOnItemSelected != null) {
-                        mOnItemSelected.onItemSelected(HorizontalListView.this, child, mLeftViewIndex + 1 + i, mAdapter.getItemId(mLeftViewIndex + 1 + i));
-                    }
-                    Log.d("onSingleTapConfirmed " , i+" mLeftViewIndex + 1 + i :" + (mLeftViewIndex + 1 + i+""));
-                    break;
-                }
-
-            }
-            return true;
-        }
-
-        @Override
-        public void onLongPress(MotionEvent e) {
-            int childCount = getChildCount();
-            for (int i = 0; i < childCount; i++) {
-                View child = getChildAt(i);
-                if (isEventWithinView(e, child)) {
-                    if (mOnItemLongClicked != null) {
-                        mOnItemLongClicked.onItemLongClick(HorizontalListView.this, child, mLeftViewIndex + 1 + i, mAdapter.getItemId(mLeftViewIndex + 1 + i));
-                    }
-                    break;
-                }
-
-            }
-        }
-
-        /**
-         * MotionEvent是否在View的范围内发生
-         * @param e
-         * @param child
-         * @return
-         */
-        private boolean isEventWithinView(MotionEvent e, View child) {
-            Rect viewRect = new Rect();
-            int[] childPosition = new int[2];
-            child.getLocationOnScreen(childPosition);
-            int left = childPosition[0];
-            int right = left + child.getWidth();
-            int top = childPosition[1];
-            int bottom = top + child.getHeight();
-            viewRect.set(left, top, right, bottom);
-            return viewRect.contains((int) e.getRawX(), (int) e.getRawY());
-        }
-    };
     private boolean mDataChanged = false;
-    private DataSetObserver mDataObserver = new DataSetObserver() {
-
-        @Override
-        public void onChanged() {
-            synchronized (HorizontalListView.this) {
-                mDataChanged = true;
-            }
-            invalidate();
-            requestLayout();
-        }
-
-        @Override
-        public void onInvalidated() {
-            reset();
-            invalidate();
-            requestLayout();
-        }
-
-    };
     private int marginEdge = -1;
     private int halfItemWidth = -1;
     private int itemWidth = -1;
+    private static final float TO_SCALE = 1.2f;
+    /**
+     * 每个item之间的距离，相当于dividetWidth
+     */
+    private static final int CHILD_MARGIN = 40;
     /**
      * 屏幕宽度的一半
      */
     private int halfScreenWidth;
+    private static final int PADDING_TOP = 30;
+
 
     public HorizontalListView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -183,9 +82,35 @@ public class HorizontalListView extends AdapterView<ListAdapter> {
         mOnItemLongClicked = listener;
     }
 
+    private DataSetObserver mDataObserver = new DataSetObserver() {
+
+        @Override
+        public void onChanged() {
+            synchronized (HorizontalListView.this) {
+                mDataChanged = true;
+            }
+            invalidate();
+            requestLayout();
+        }
+
+        @Override
+        public void onInvalidated() {
+            reset();
+            invalidate();
+            requestLayout();
+        }
+
+    };
+
     @Override
     public ListAdapter getAdapter() {
         return mAdapter;
+    }
+
+    @Override
+    public View getSelectedView() {
+        //TODO: implement
+        return null;
     }
 
     @Override
@@ -196,12 +121,6 @@ public class HorizontalListView extends AdapterView<ListAdapter> {
         mAdapter = adapter;
         mAdapter.registerDataSetObserver(mDataObserver);
         reset();
-    }
-
-    @Override
-    public View getSelectedView() {
-        //TODO: implement
-        return null;
     }
 
     private synchronized void reset() {
@@ -241,6 +160,7 @@ public class HorizontalListView extends AdapterView<ListAdapter> {
         child.measure(MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.AT_MOST),
                 MeasureSpec.makeMeasureSpec(getHeight(), MeasureSpec.AT_MOST));
     }
+
 
     /**
      * 滑动过程中会一直调用
@@ -414,22 +334,7 @@ public class HorizontalListView extends AdapterView<ListAdapter> {
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
         boolean handled = super.dispatchTouchEvent(ev);
-
-        boolean gestureHandled = false;
-        int action = ev.getAction();
-        switch (action) {
-            case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_MOVE:
-                gestureHandled = mGesture.onTouchEvent(ev);
-                break;
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_CANCEL:
-                gestureHandled = mGesture.onTouchEvent(ev);
-                //todo 这里判断是否还在滑动
-                Log.i("zhaoxin","test");
-                break;
-        }
-        handled |= gestureHandled;
+        handled |= mGesture.onTouchEvent(ev);
         return handled;
     }
 
@@ -447,6 +352,90 @@ public class HorizontalListView extends AdapterView<ListAdapter> {
         mScroller.forceFinished(true);
         return true;
     }
+
+    private OnGestureListener mOnGesture = new GestureDetector.SimpleOnGestureListener() {
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return HorizontalListView.this.onDown(e);
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                               float velocityY) {
+            return HorizontalListView.this.onFling(e1, e2, velocityX, velocityY);
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                float distanceX, float distanceY) {
+
+            synchronized (HorizontalListView.this) {
+                mNextX += (int) distanceX;
+            }
+            requestLayout();
+
+            return true;
+        }
+
+        /**
+         * 点击动作执行完毕时会调用
+         * @param e
+         * @return
+         */
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            for (int i = 0; i < getChildCount(); i++) {
+                View child = getChildAt(i);
+                if (isEventWithinView(e, child)) {
+                    if (mOnItemClicked != null) {
+                        mOnItemClicked.onItemClick(HorizontalListView.this, child, mLeftViewIndex + 1 + i, mAdapter.getItemId(mLeftViewIndex + 1 + i));
+                    }
+//                    setSelection(mLeftViewIndex + 1 + i);
+                    if (mOnItemSelected != null) {
+                        mOnItemSelected.onItemSelected(HorizontalListView.this, child, mLeftViewIndex + 1 + i, mAdapter.getItemId(mLeftViewIndex + 1 + i));
+                    }
+                    Log.d("onSingleTapConfirmed ", i + " mLeftViewIndex + 1 + i :" + (mLeftViewIndex + 1 + i + ""));
+                    break;
+                }
+
+            }
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            int childCount = getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View child = getChildAt(i);
+                if (isEventWithinView(e, child)) {
+                    if (mOnItemLongClicked != null) {
+                        mOnItemLongClicked.onItemLongClick(HorizontalListView.this, child, mLeftViewIndex + 1 + i, mAdapter.getItemId(mLeftViewIndex + 1 + i));
+                    }
+                    break;
+                }
+
+            }
+        }
+
+        /**
+         * MotionEvent是否在View的范围内发生
+         * @param e
+         * @param child
+         * @return
+         */
+        private boolean isEventWithinView(MotionEvent e, View child) {
+            Rect viewRect = new Rect();
+            int[] childPosition = new int[2];
+            child.getLocationOnScreen(childPosition);
+            int left = childPosition[0];
+            int right = left + child.getWidth();
+            int top = childPosition[1];
+            int bottom = top + child.getHeight();
+            viewRect.set(left, top, right, bottom);
+            return viewRect.contains((int) e.getRawX(), (int) e.getRawY());
+        }
+    };
 
     private void scaleChild() {
         int childCount = getChildCount();
